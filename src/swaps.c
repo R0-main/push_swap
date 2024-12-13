23fd2e9b5d64d61d28d675253e6ccd54fc96d1b9
R0-main <justromixx@gmail.com>
2024-12-13 15:12:08 +0100

    refactoring swap + added swap_both (ss)

--- src/swaps.c
+++ src/swaps.c
@@ -6,37 +6,38 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:36:51 by rguigneb          #+#    #+#             */
-/*   Updated: 2024/12/13 12:58:49 by rguigneb         ###   ########.fr       */
+/*   Updated: 2024/12/13 15:11:22 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	swap_a(t_dllist **a)
+static int swap(t_dllist **x)
 {
 	t_dllist	*tmp;
 
-	if (!(*a) || !(*a)->next)
+	if (!(*x) || !(*x)->next)
 		return (0);
-	tmp = (*a)->next;
+	tmp = (*x)->next;
 	tmp->prev = NULL;
-	(*a)->next = tmp->next;
-	tmp->next = *a;
-	(*a)->prev = tmp;
-	*a = tmp;
+	(*x)->next = tmp->next;
+	tmp->next = *x;
+	(*x)->prev = tmp;
+	*x = tmp;
 	return (0);
+}
+
+int	swap_a(t_dllist **a)
+{
+	return (swap(a));
 }
 int	swap_b(t_dllist **b)
 {
-	t_dllist	*tmp;
+	return (swap(b));
+}
 
-	if (!(*b) || !(*b)->next)
-		return (0);
-	tmp = (*b)->next;
-	tmp->prev = NULL;
-	(*b)->next = tmp->next;
-	tmp->next = *b;
-	(*b)->prev = tmp;
-	*b = tmp;
-	return (0);
+int	swap_in_both(t_dllist **a, t_dllist **b)
+{
+	swap_a(a);
+	swap_b(b);
 }
